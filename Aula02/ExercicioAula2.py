30d94955b78f724dc318c7c45617a8f747c8b4ce
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-03-20 08:52:59 -0300

    Update ExercicioAula2.py

--- Aula02/ExercicioAula2.py
+++ Aula02/ExercicioAula2.py
@@ -11,3 +11,9 @@ print("Numero de funções compartilhadas:",compartilhadas)
 
 #A função "Len" é utilizada para contar a quantidade de coisas presentes na lista
 
+
+## Problema: Se a divisão resultar em um número decimal (ex: 5/2 = 2,5), a parte decimal será truncada, pois 'resultado' é um inteiro
+## Pergunta para reflexão: Como podemos modificar o código para que a divisão retorne um valor real corretamente?
+
+## Resposta: Para que a divisão retorne um valor corretamente, mantendo as boas práticas de programação e buscando deixar otimizado seria -
+## - possível mudar apenas o tipo da variavel resultado para float(real), assim os valores de entrada continuam int(inteiro) 
