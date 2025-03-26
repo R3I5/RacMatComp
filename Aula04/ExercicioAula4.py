d7291118ee5a020a2cc4df58c1a6c7ee816d96aa
João Victor dos Reis da Silva <j.victor.reis20044@gmail.com>
2025-03-26 11:25:54 -0300

    Exercicio Aula 04

---
+++ Aula04/ExercicioAula4.py
@@ -0,0 +1,7 @@
+#Crie um programa em python que calcula o valor da seguinte expressão matemática:
+
+a = float(input("Insira o valor de A:"))
+b = float(input("Insira o valor de B:"))
+c = float(input("Insira o valor de C:"))
+d = float(input("Insira o valor de D:"))
+G = (a * b + (c**2))/((a+b)**(1/d)+c)
